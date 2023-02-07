e2fe99178fd7b74c5017ef4cdd17d49616196593
KiritoVV <113103472+KiritoVV@users.noreply.github.com>
2023-02-07 18:21:27 +0000

    added a key door script

--- Assets/scripts/KeyDoorController.cs
+++ Assets/scripts/KeyDoorController.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyDoorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator doorAnim;
+    private bool doorOpen = false;
+
+    [Header("Animation Names")]
+    [SerializeField] private string openAnimationName = "DoorOpen";
+    [SerializeField] private string closeAnimationName = "DoorClose";
+
+    [SerializeField] private int timeToShowUI = 1;
+    [SerializeField] private GameObject showDoorLockedUI = null;
+
+    [SerializeField] private KeyInventory1 _keyInventory1 = null;
+
+    [SerializeField] private int waitTimer = 1;
+    [SerializeField] private bool pauseInteraction = false;
+
+    private void Awake()
     {
-        
+        doorAnim = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PauseDoorInteraction()
     {
-        
+        pauseInteraction = true;
+        yield return new WaitForSeconds(waitTimer);
+        pauseInteraction = false;
     }
+
+    public void PlayAnimation()
+    {
+        if(_keyInventory1.hasGreyKey)
+        {
+            if(!doorOpen && !pauseInteraction)
+            {
+                doorAnim.Play(openAnimationName, 0, 0.0f);
+                doorOpen = true;
+                StartCoroutine(PauseDoorInteraction());
+            }
+
+            else if (doorOpen && !pauseInteraction)
+            {
+                doorAnim.Play(closeAnimationName, 0, 0.0f);
+                doorOpen = false;
+                StartCoroutine(PauseDoorInteraction());
+            }
+        }
+        else
+        {
+            StartCoroutine(ShowDoorLocked());
+        }
+    }
+
+    IEnumerator ShowDoorLocked()
+    {
+        showDoorLockedUI.SetActive(true);
+        yield return new WaitForSeconds(timeToShowUI);
+        showDoorLockedUI.SetActive(false);
+    }
+
 }
