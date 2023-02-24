8d621e01eb5f7566a1eae18dd2c96c893e515a9b
KiritoVV <113103472+KiritoVV@users.noreply.github.com>
2023-02-24 15:31:14 +0000

    ADDED A AUDIO FOR THE MONSTER AND A KILL CUTSCENE

--- Assets/scripts/KillBlock.cs
+++ Assets/scripts/KillBlock.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class KillBlock : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawn_point;
+
+    public RawImage Jumpscare;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,6 +20,7 @@ public class KillBlock : MonoBehaviour
     }
     IEnumerator Teleporter()
     {
+        Jumpscare.gameObject.SetActive(true);
         yield return new WaitForSeconds(10f);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
