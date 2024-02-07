216de57e5bf68ae24518ea00bc8c2a13a1cc85e7
Innominate-Dev <InnominateDev@gmail.com>
2024-02-07 23:24:02 +0000

    Lever script working just need to add more levers and door opening

--- Assets/scripts/LeverScript.cs
+++ Assets/scripts/LeverScript.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class LeverScript : MonoBehaviour
 {
     public GameObject interaction_text;
     public GameObject crosshair;
     public Transform lever;
+
+    private AudioSource leverSFX;
     private bool isPulled;
 
     // Start is called before the first frame update
     void Start()
     {
+        leverSFX = GetComponent<AudioSource>();
         interaction_text.SetActive(false);
     }
 
@@ -27,26 +28,18 @@ public class LeverScript : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            interaction_text.SetActive(true) ;
-
-            if (isPulled == false && Input.GetKeyDown(KeyCode.E))
+            if (isPulled == false)
             {
-                isPulled = true;
+                interaction_text.SetActive(true);
 
-                lever.Rotate(Time.deltaTime * 0, -60, 0);
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    isPulled = true;
+                    leverSFX.Play();
+                    lever.Rotate(Time.deltaTime * 0, -60, 0);
 
-                interaction_text.SetActive(false);
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            if(isPulled == false)
-            {
-                interaction_text.SetActive(false);
+                    interaction_text.SetActive(false);
+                }
             }
         }
     }
